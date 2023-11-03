91140515d6f5f61edd5ad3c2bc17d9c0c11d89ea
SuperxJoker <oaidaalexia@yahoo.com>
2023-11-04 01:46:55 +0200

    SQLite database set up in spring

--- backendApp/src/main/java/com/example/backendapp/BackendAppApplication.java
+++ backendApp/src/main/java/com/example/backendapp/BackendAppApplication.java
@@ -2,12 +2,13 @@ package com.example.backendapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class BackendAppApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendAppApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAppApplication.class, args);
+	}
 
 }
---
+++ backendApp/src/main/java/com/example/backendapp/model/User.java
@@ -0,0 +1,30 @@
+package com.example.backendapp.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+}
