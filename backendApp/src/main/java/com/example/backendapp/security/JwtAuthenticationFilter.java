ab03d19b345b0e0ef7f7f7f55129e7df42f3b871
SuperxJoker <oaidaalexia@yahoo.com>
2023-11-14 15:47:15 +0200

    Configured the jwt service, created the filter, added necessary security

---
+++ backendApp/src/main/java/com/example/backendapp/security/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.example.backendapp.security;
+
+import com.example.backendapp.exceptions.JwtAuthenticationException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        try {
+            if (authHeader != null && authHeader.startsWith("Bearer ")) {
+                String jwt = authHeader.substring(7);
+                Authentication auth = jwtService.getAuthentication(jwt);
+                SecurityContextHolder.getContext().setAuthentication(auth);
+                filterChain.doFilter(request, response);
+            } else {
+                filterChain.doFilter(request, response);
+            }
+        } catch (final JwtAuthenticationException ex) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error parsing JWT Token");
+        }
+    }
+
+}
