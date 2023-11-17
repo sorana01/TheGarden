c7db298e31528d4545e9ae43ec7ad69440654f09
SuperxJoker <oaidaalexia@yahoo.com>
2023-11-17 22:19:51 +0200

    Did the registration on frontend Linked to backend Added functionality to login button Changed in AndroidManifest.xml the start up xml Started also the LoginActivity

---
+++ mobileApp/app/src/main/java/com/example/thegarden/authentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.thegarden.authentication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.thegarden.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login); // Set the content view to your login layout
+    }
+}
---
+++ mobileApp/app/src/main/java/com/example/thegarden/network/ApiClient.java
@@ -0,0 +1,26 @@
+package com.example.thegarden.network;
+
+import com.google.gson.Gson;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+    private Retrofit retrofit;
+
+    public ApiClient(){
+        initializeRetrofit();
+    }
+
+    private void initializeRetrofit(){
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://192.168.1.230:8080")
+                .addConverterFactory(GsonConverterFactory.create(new Gson()))
+                .build();
+    }
+
+    public Retrofit getRetrofit() {
+        return retrofit;
+    }
+
+}
