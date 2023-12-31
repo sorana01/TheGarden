b07f42eab39ccd7f0c5fd206bb4b343eb58be7ec
Doris Sorana Haiduc <doris.haiduc@student.upt.ro>
2023-12-31 20:40:13 +0200

    Created all necessary fragments

--- mobileApp/app/src/main/java/com/example/thegarden/MainActivity.java
+++ mobileApp/app/src/main/java/com/example/thegarden/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_plants, R.id.navigation_scan, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
@@ -69,13 +69,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-        binding.yourButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                identifyPlant.identifyPlant(R.drawable.plant1);
-            }
-        });
     }
 
     private void dispatchTakePictureIntent() {
--- mobileApp/app/src/main/java/com/example/thegarden/ui/scan/ScanViewModel.java
+++ mobileApp/app/src/main/java/com/example/thegarden/ui/scan/ScanViewModel.java
@@ -1,16 +1,16 @@
-package com.example.thegarden.ui.dashboard;
+package com.example.thegarden.ui.scan;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class ScanViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public ScanViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is scan fragment");
     }
 
     public LiveData<String> getText() {
